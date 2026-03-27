5ebeddf3d27bf3cdba8463d3591a70c57452f299
GSajellac <gabrielcallejas396@gmail.com>
2026-03-27 16:04:23 -0300

    Add files via upload

---
+++ 8_callejas_/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+            Console.WriteLine("Hola wachin");
+            Console.Write("como te llamas wachin?: ");
+            nombre = Console.ReadLine();
+            Console.Write("cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+            Console.WriteLine("hola " + nombre + ".Tu pelicula favorita " + pelicula + " es una porqueria.");
+        }
+    }
+}
