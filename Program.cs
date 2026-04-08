7b5ec4d30b90ee0831ab9101ad90c1e02097026d
GSajellac <gabrielcallejas396@gmail.com>
2026-04-08 16:36:59 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,29 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-         Console.Write("cantidad de midiclorianos");
-         int n = Convert.ToInt32(Console.ReadLine());
-         if (n > 15000)
-            {
-             Console.WriteLine("Potencial Maestro Jedi detectado. Avisar al Consejo");
-            }
-         else
-            {
-             if (n > 4999 & n < 1501)
-                {
-                 Console.WriteLine("Aceptable para entrenamiento Padawan");
-                }
-                else
-                {
-                 Console.WriteLine("No posee sensibilidad a la Fuerza");
-                }
-            }
+            Console.WriteLine("estacionemiento EL CHIQUI TAPIA");
+            Console.Write("minutos en el estacionamiento: ");
+            int minutos = Convert.ToInt32(Console.ReadLine());
+            double horas = (double)minutos / 60;
+            Console.Write.Line("horas en el estacionamiento " + horas);
+
         }
     }
 }
