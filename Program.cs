996a02b95b51496be2a6885e139dbb6c07b88d5e
GSajellac <gabrielcallejas396@gmail.com>
2026-04-08 16:33:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("los kg son ");
+         int kg = Convert.ToInt32(Console.ReadLine());
+         int resto = kg - 5000;
+         if (kg <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado. Buen viaje, mando");
+            }
+         else
+            {
+             Console.WriteLine("Alerta: Exceso de peso.Debes descargar " + resto +  " kg para despegar");
+            }
+        }
+    }
+}
