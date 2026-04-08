9f89804e7eab11221651c92cf5b7407a69580aaa
GSajellac <gabrielcallejas396@gmail.com>
2026-04-08 16:38:35 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
     class Program
     {
@@ -13,9 +13,13 @@ namespace ConsoleApplication1
             Console.WriteLine("estacionemiento EL CHIQUI TAPIA");
             Console.Write("minutos en el estacionamiento: ");
             int minutos = Convert.ToInt32(Console.ReadLine());
-            double horas = (double)minutos / 60;
-            Console.Write.Line("horas en el estacionamiento " + horas);
-
+            int horas = minutos / 60;
+            int resto = minutos - 60 * horas;
+            int ph = horas * 6000;
+            int mh = minutos * 150;
+            int to = mh + ph;
+            int total = (to * 21) / 100 ;
+            Console.WriteLine("horas en el estacionamiento " + horas + " minutos " + resto + " el total a pagar es: " + total );
         }
     }
 }
